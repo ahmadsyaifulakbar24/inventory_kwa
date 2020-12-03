73da75b13e1be4479c9e7de8e07a61fb73aaad00
ipulbelcram <ipulbelcram@gmail.com>
2020-12-03 15:13:02 +0700

    change detail alker resource

--- inventory_kwa_api/app/Http/Resources/Alker/DetailAlkerResource.php
+++ inventory_kwa_api/app/Http/Resources/Alker/DetailAlkerResource.php
@@ -12,18 +12,18 @@ class DetailAlkerResource extends JsonResource
 {
     public function toArray($request)
     {
-        $sto = Param::find($this->detail_alker->sto_id);
-        $teknisi = Employee::find($this->detail_alker->teknisi_id);
+        !empty($this->detail_alker->sto_id) ? $sto = Param::find($this->detail_alker->sto_id) : $sto = '';
+        !empty($this->detail_alker->teknisi_id) ? $teknisi = Employee::find($this->detail_alker->teknisi_id) : $teknisi = '';
         return [
             'id' => $this->id,
             'main_alker' => new MainAlkerResource($this->main_alker),
             'kode_alker' => $this->kode_alker,
             'sto' => [
-                'id' => $sto->id,
-                'sto' => $sto->param
+                'id' => !empty($sto->id) ? $sto->id : '',
+                'sto' => !empty($sto->param) ? $sto->param : ''
             ],
-            'teknisi' => new EmployeeResource($teknisi),
-            'kegunaan' => $this->detail_alker->kegunaan,
+            'teknisi' => !empty($teknisi) ? new EmployeeResource($teknisi) : '',
+            'kegunaan' => !empty($this->detail_alker->kegunaan) ? $this->detail_alker->kegunaan : '',
         ];
     }
 }
