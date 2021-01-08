6e36eda2388a07c8bfe7c99157da105ba383d807
ipulbelcram <ipulbelcram@gmail.com>
2021-01-08 15:54:41 +0700

    add total alker in main alker

--- inventory_kwa_api/app/Http/Resources/Alker/MainAlkerResource.php
+++ inventory_kwa_api/app/Http/Resources/Alker/MainAlkerResource.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Resources\Alker;
 
+use App\Models\Alker;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class MainAlkerResource extends JsonResource
 {
     public function toArray($request)
     {
+        $main_alker = $this->id;
+        $alker_total = Alker::where('main_alker_id', $main_alker)->count();
         return [
             'id' => $this->id,
             'kode_main_alker' => $this->kode_main_alker,
             'nama_barang' => $this->nama_barang,
-            'satuan' => $this->satuan
+            'satuan' => $this->satuan,
+            'total_alker_gudang' => !empty($alker_total) ? $alker_total : 0,
         ];
     }
 }
