0bab7f8e4099c862a1fe5d6cfc9adca157d50717
ipulbelcram <ipulbelcram@gmail.com>
2021-01-20 15:05:49 +0700

    add total alker keluar di main alker resource

--- inventory_kwa_api/app/Http/Resources/Alker/MainAlkerResource.php
+++ inventory_kwa_api/app/Http/Resources/Alker/MainAlkerResource.php
@@ -10,13 +10,15 @@ class MainAlkerResource extends JsonResource
     public function toArray($request)
     {
         $main_alker = $this->id;
-        $alker_total = Alker::where('main_alker_id', $main_alker)->whereIn('status', ['in','pending'])->count();
+        $alker_gudang = Alker::where('main_alker_id', $main_alker)->whereIn('status', ['in','pending'])->count();
+        $alker_keluar = Alker::where('main_alker_id', $main_alker)->whereIn('status', 'out')->count();
         return [
             'id' => $this->id,
             'kode_main_alker' => $this->kode_main_alker,
             'nama_barang' => $this->nama_barang,
             'satuan' => $this->satuan,
-            'total_alker_gudang' => !empty($alker_total) ? $alker_total : 0,
+            'total_alker_keluar' => !empty($alker_keluar) ? $alker_keluar : 0,
+            'total_alker_gudang' => !empty($alker_gudang) ? $alker_gudang : 0,
         ];
     }
 }
