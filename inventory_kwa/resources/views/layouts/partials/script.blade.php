921f7102fc924e5dd3be12d853fdbaf8ff2fdd8e
Hilmi <nurhilmi.mail@gmail.com>
2021-02-02 00:09:27 +0700

    change api url

--- inventory_kwa/resources/views/layouts/partials/script.blade.php
+++ inventory_kwa/resources/views/layouts/partials/script.blade.php
@@ -2,4 +2,4 @@
 	<script src="{{asset('assets/vendors/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 	<script src="{{asset('assets/js/main.js')}}"></script>
 	<script>const root = '{{Request::root()}}/'</script>
-	<script>const api_url = 'http://103.112.44.35/inventory_kwa/inventory_kwa_api/public/'</script>
+	<script>const api_url = 'http://api.inventory.lekarlwig.com/'</script>
