014969f5e8edeb3d49cef450eb8df950abb70794
Hilmi <nurhilmi.mail@gmail.com>
2021-02-02 00:44:24 +0700

    view tool (add function pagination)

--- inventory_kwa/public/api/view-tool.js
+++ inventory_kwa/public/api/view-tool.js
@@ -71,76 +71,7 @@ function get_alker(page) {
                         $('#linkQR' + value.id).attr('onClick', 'downloadQR(' + value.id + ')')
                     }, 0)
                 })
-
-                let links = result.links
-				let meta = result.meta
-				let current = meta.current_page
-
-				let first = links.first.replace('http://103.112.44.35/inventory_kwa/inventory_kwa_api/public/alker/get?page=','')
-				if(first != current){
-					$('#first').removeClass('disabled')
-					$('#first').data('id',first)
-				} else {
-					$('#first').addClass('disabled')
-				}
-
-				if(links.prev != null){
-					$('#prev').removeClass('disabled')
-					let prev = links.prev.replace('http://103.112.44.35/inventory_kwa/inventory_kwa_api/public/alker/get?page=','')
-					$('#prev').data('id',prev)
-
-					$('#prevCurrent').show()
-					$('#prevCurrent span').html(prev)
-					$('#prevCurrent').data('id',prev)
-					
-					let prevCurrentDouble = prev - 1
-					if(prevCurrentDouble > 0) {
-						$('#prevCurrentDouble').show()
-						$('#prevCurrentDouble span').html(prevCurrentDouble)
-						$('#prevCurrentDouble').data('id',prevCurrentDouble)
-					} else {
-						$('#prevCurrentDouble').hide()
-					}
-				} else {
-					$('#prev').addClass('disabled')
-					$('#prevCurrent').hide()
-					$('#prevCurrentDouble').hide()
-				}
-
-				$('#current').addClass('active')
-				$('#current span').html(current)
-
-				if(links.next != null){
-					$('#next').removeClass('disabled')
-					let next = links.next.replace('http://103.112.44.35/inventory_kwa/inventory_kwa_api/public/alker/get?page=','')
-					$('#next').data('id',next)
-
-					$('#nextCurrent').show()
-					$('#nextCurrent span').html(next)
-					$('#nextCurrent').data('id',next)
-								
-					let nextCurrentDouble = ++next
-					if(nextCurrentDouble <= meta.last_page) {
-						$('#nextCurrentDouble').show()
-						$('#nextCurrentDouble span').html(nextCurrentDouble)
-						$('#nextCurrentDouble').data('id',nextCurrentDouble)
-					} else {
-						$('#nextCurrentDouble').hide()
-					}
-				} else {
-					$('#next').addClass('disabled')
-					$('#nextCurrent').hide()
-					$('#nextCurrentDouble').hide()
-				}
-
-				let last = links.last.replace('http://103.112.44.35/inventory_kwa/inventory_kwa_api/public/alker/get?page=','')
-				if(last != current){
-					$('#last').removeClass('disabled')
-					$('#last').data('id',last)
-				} else {
-					$('#last').addClass('disabled')
-				}
-
+                pagination(result.links, result.meta, result.meta.path)
                 $('#stok').html(result.meta.total)
 				$('#pagination').show()
 				$('#loading_data').hide()
