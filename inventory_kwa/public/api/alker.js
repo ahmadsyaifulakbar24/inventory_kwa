49f94ee20e12c39ee0753f3f3932222c06e6ad61
Hilmi <nurhilmi.mail@gmail.com>
2021-02-02 11:33:25 +0700

    approve alker & salker (add button reject)

--- inventory_kwa/public/api/alker.js
+++ inventory_kwa/public/api/alker.js
@@ -20,8 +20,7 @@ function get_alker_request_group(page) {
                 $('#data').removeClass('hide')
                 let append
                 $.each(result.data, function(index, value) {
-                    append =
-                        `<tr data-id="${value.id}" data-alker="${value.kode_alker}">
+                    append = `<tr data-id="${value.id}" data-alker="${value.kode_alker}">
 						<td><i class="mdi mdi-check mdi-checkbox-blank-outline mdi-18px pr-0" role="button"></i></td>
 						<td class="text-truncate"><a href="${root}alker/${value.alker_id}">${value.kode_alker}</a></td>
 						<td class="text-truncate">${value.nama_barang}</td>
@@ -126,7 +125,7 @@ function get_alker_request(page) {
             $('#pagination').show()
             $('#loading_data_get_alker_request').hide()
             if (result.data.length > 0) {
-                let append, cancel, front, back, sto, teknisi, kegunaan
+                let append, color, front, back, sto, teknisi, kegunaan
                 $.each(result.data, function(index, value) {
                     value.front_picture == '' || value.front_picture == null ? front = 'd-none' : front = 'd-block'
                     value.back_picture == '' || value.back_picture == null ? back = 'd-none' : back = 'd-block'
@@ -134,17 +133,13 @@ function get_alker_request(page) {
                     value.teknisi != null ? teknisi = value.teknisi.name : teknisi = ''
                     value.kegunaan != null ? kegunaan = value.kegunaan.toUpperCase() : kegunaan = ''
                     if (value.status == 'accepted') {
-                        success = 'text-success'
-                        cancel = ''
+                        color = 'text-success'
                     } else if (value.status == 'pending') {
-                        success = 'text-warning d-flex align-items-center'
-                        cancel = `<div class="btn btn-sm btn-warning cancel ml-2" data-id="${value.id}" data-kode="${value.alker.kode_alker}" data-alker="${value.alker.main_alker.nama_barang}">Cancel</div>`
+                        color = 'text-warning'
                     } else {
-                        success = 'text-danger'
-                        cancel = ''
+                        color = 'text-danger'
                     }
-                    append =
-                        `<tr data-id="${value.id}" data-alker="${value.alker.kode_alker}">
+                    append = `<tr data-id="${value.id}" data-alker="${value.alker.kode_alker}">
 						<td><i class="mdi mdi-check mdi-checkbox-blank-outline mdi-18px pr-0" role="button"></i></td>
 						<td class="text-truncate"><a href="${root}alker/detail/${btoa(value.alker.kode_alker)}">${value.alker.kode_alker}</a></td>
 						<td class="text-truncate">${value.alker.main_alker.nama_barang}</td>
@@ -152,7 +147,7 @@ function get_alker_request(page) {
 						<td class="text-truncate">${teknisi}</td>
 						<td>${kegunaan}</td>
 						<td class="text-truncate">${value.keterangan.keterangan}</td>
-						<td class="text-capitalize ${success}" id="status${value.id}">${value.status + cancel}</td>
+						<td class="text-capitalize ${color}">${value.status}</td>
 						<td><a href="${value.front_picture}" class="btn btn-sm btn-outline-primary text-truncate ${front}" target="_blank">Depan</a></td>
 						<td><a href="${value.back_picture}" class="btn btn-sm btn-outline-primary text-truncate ${back}" target="_blank">Belakang</a></td>
 					</tr>`
@@ -187,35 +182,6 @@ $(document).on('click', '.page', function() {
             get_alker_request(page)
         }
     }
-})
-
-$(document).on('click', '.cancel', function() {
-    let id = $(this).data('id')
-    let kode = $(this).data('kode')
-    let alker = $(this).data('alker')
-    $('#cancel').attr('data-id', id)
-    $('#modal-cancel').modal('show')
-    $('.modal-body').html(`Anda yakin ingin membatalkan request <b>${kode}</b> (${alker})?`)
-})
-
-$(document).on('click', '#cancel', function() {
-    let id = $(this).data('id')
-    $('#cancel').attr('disabled', true)
-    $.ajax({
-        url: api_url + 'alker/reject_alker_request/' + id,
-        type: 'GET',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Bearer " + token)
-        },
-        success: function(result) {
-            $('#status' + id).html('Rejected')
-            $('#status' + id).removeClass('d-flex')
-            $('#status' + id).addClass('text-danger')
-            $('#status' + id).removeClass('text-warning')
-            $('#cancel').attr('disabled', false)
-            $('#modal-cancel').modal('hide')
-        }
-    })
 })
 
 $('#filter').change(function() {
--- inventory_kwa/public/api/approve-alker.js
+++ inventory_kwa/public/api/approve-alker.js
@@ -5,7 +5,7 @@ function get_alker_request(page) {
         url: api_url + 'alker/get_alker_request',
         type: 'GET',
         data: {
-        	page: page
+            page: page
         },
         beforeSend: function(xhr) {
             xhr.setRequestHeader("Authorization", "Bearer " + token)
@@ -13,26 +13,25 @@ function get_alker_request(page) {
         success: function(result) {
             // console.log(result.data)
             $('#loading').addClass('hide')
-			$('#pagination').show()
-			$('#loading_data_get_alker_request').hide()
+            $('#pagination').show()
+            $('#loading_data_get_alker_request').hide()
             if (result.data.length > 0) {
                 $('#data').removeClass('hide')
-                let append, status, success, front, back, sto, teknisi, kegunaan
+                let append, color, front, back, sto, teknisi, kegunaan
                 $.each(result.data, function(index, value) {
-                	value.sto.sto != null ? sto = value.sto.sto : sto = ''
-                	value.teknisi != null ? teknisi = value.teknisi.name : teknisi = ''
-                	value.kegunaan != null ? kegunaan = value.kegunaan.toUpperCase() : kegunaan = ''
-                	value.front_picture == '' ? front = 'd-none' : front = 'd-block'
-                	value.back_picture == '' ? back = 'd-none' : back = 'd-block'
+                    value.sto.sto != null ? sto = value.sto.sto : sto = ''
+                    value.teknisi != null ? teknisi = value.teknisi.name : teknisi = ''
+                    value.kegunaan != null ? kegunaan = value.kegunaan.toUpperCase() : kegunaan = ''
+                    value.front_picture == '' ? front = 'd-none' : front = 'd-block'
+                    value.back_picture == '' ? back = 'd-none' : back = 'd-block'
                     if (value.status == 'accepted') {
-                    	success = 'text-success'
-                    	status = 'Disetujui'
+                        color = 'text-success'
+                    } else if (value.status == 'pending') {
+                        color = 'text-warning'
                     } else {
-                    	success = 'text-warning'
-                    	status = value.status
+                        color = 'text-danger'
                     }
-                    append =
-                        `<tr data-id="${value.id}" data-alker="${value.alker.kode_alker}">
+                    append = `<tr data-id="${value.id}" data-alker="${value.alker.kode_alker}">
 						<td><i class="mdi mdi-check mdi-checkbox-blank-outline mdi-18px pr-0" role="button"></i></td>
 						<td class="text-truncate"><a href="${root}approve-alker/${value.id}">${value.alker.kode_alker}</a></td>
 						<td class="text-truncate">${value.alker.main_alker.nama_barang}</td>
@@ -40,7 +39,7 @@ function get_alker_request(page) {
 						<td class="text-truncate">${teknisi}</td>
 						<td>${kegunaan}</td>
 						<td class="text-truncate">${value.keterangan.keterangan}</td>
-						<td class="text-capitalize ${success}">${status}</td>
+						<td class="text-capitalize ${color}">${value.status}</td>
 						<td><a href="${value.front_picture}" class="btn btn-sm btn-outline-primary text-truncate ${front}" target="_blank">Depan</a></td>
 						<td><a href="${value.back_picture}" class="btn btn-sm btn-outline-primary text-truncate ${back}" target="_blank">Belakang</a></td>
 					</tr>`
@@ -60,11 +59,11 @@ function get_alker_request(page) {
 }
 
 $('.page').click(function() {
-	if(!$(this).is('.active, .disabled')){
-		let page = $(this).data('id')
-		$('#pagination').hide()
-		$('#loading_data_get_alker_request').show()
-		$('#data_get_alker_request').html('')
-		get_alker_request(page)
-	}
+    if (!$(this).is('.active, .disabled')) {
+        let page = $(this).data('id')
+        $('#pagination').hide()
+        $('#loading_data_get_alker_request').show()
+        $('#data_get_alker_request').html('')
+        get_alker_request(page)
+    }
 })
